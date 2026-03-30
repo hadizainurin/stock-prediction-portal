990ad67af72b1c92f3b9806c674c2a27597241b5
hadizainurin <hadi@ttvision-tech.com>
2026-03-30 17:10:11 +0800

    registration page

--- backend-drf/accounts/serializers.py
+++ backend-drf/accounts/serializers.py
@@ -1,11 +1,17 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, style={'input_type': 'password'})
     class Meta:
         model = User
         fields = ['username', 'email','password']
+
+    # We will not force the password minimum validation here
+    # def validate_password(self, value):
+    #     validate_password(value)
+    #     return value
 
     def create(self, validated_data):
         # User.objects.create = save the password in a plain text
@@ -15,4 +21,5 @@ class UserSerializer(serializers.ModelSerializer):
             validated_data['email'],
             validated_data['password']
         ) # You can also pass validated_data as **validated_data only if the fields in the serializer are the same as the fields in the model
+        # user = User.objects.create_user(**validated_data)
         return user
--- frontend-react/src/components/Button.jsx
+++ frontend-react/src/components/Button.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Button = (props) => {
   return (
     <>
         {/* in react we don't use href, instead we use link component to router its */}
-        <a className={`btn ${props.class}`} href='#'>{props.text}</a>
+        <Link className={`btn ${props.class}`} to={props.url}>{props.text}</Link>
     </>
   )
 }
